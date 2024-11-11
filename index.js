051677e2c148b7258dbac49ce1aab562caafab23
Thehax1sout <mmahmoudgamal76@gmail.com>
2024-11-11 17:10:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["👀 Keeping an eye on things", "🛡️ Ensuring order in the server", "🔍 Watching for rule-breakers", "🛠️ Ensuring a safe community"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
